a9f10b5ea3119e90132ec858f2dcaf0c08b69d42
donovilia2007 <57286242+donovilia2007@users.noreply.github.com>
2023-11-06 21:43:59 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import logging
+import os
 
 from aiogram import Bot, Dispatcher
 from aiogram.enums.parse_mode import ParseMode
@@ -8,7 +9,7 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from functional import router
 
 async def main():
-    bot = Bot(token=TOKEN, parse_mode=ParseMode.HTML)
+    bot = Bot(token=os.environ.get(TOKEN), parse_mode=ParseMode.HTML)
     dp = Dispatcher(storage=MemoryStorage())
     dp.include_router(router)
     await bot.delete_webhook(drop_pending_updates=True)
