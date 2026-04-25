dfd32feafa12672351b7f9a63779edf748d603cb
Ilia Donov <donov.ia@phystech.edu>
2026-04-26 01:33:45 +0300

    add main.py, santa.py, requirments.txt, update README.md

---
+++ main.py
@@ -0,0 +1,14 @@
+import asyncio
+import logging
+
+from santa import router, bot, dp, SantaGame
+
+async def main():
+    dp.include_router(router)
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot, allowed_updates=dp.resolve_used_update_types())
+    await SantaGame.init_db()
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
