9c80edbdddf51bdf579aee3c4d2f6230808d17e5
Ilia Donov <donov.ia@phystech.edu>
2026-05-10 10:17:15 +0300

    fix open TOKEN.txt

--- src/start_aiogram.py
+++ src/start_aiogram.py
@@ -2,7 +2,7 @@ from aiogram import Router, Dispatcher, Bot
 from aiogram.fsm.storage.memory import MemoryStorage
 
 router = Router()
-f = open("../TOKEN.txt")
+f = open("TOKEN.txt")
 token = f.read()
 dp = Dispatcher(storage=MemoryStorage())
 bot = Bot(token=token)
